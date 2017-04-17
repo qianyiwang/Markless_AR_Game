1d78d77d59aa82495f7ce88558dce42f312b8c66
qianyiwang <stevenwang473328@gmail.com>
2017-04-16 21:48:58 -0400

    complete

---
+++ Assets/Scripts/collisionScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col){
+		GameObject explosion = Instantiate (Resources.Load("FireMobile", typeof(GameObject))) as GameObject;
+		explosion.transform.position = transform.position;
+		Destroy (col.gameObject);
+		Destroy (explosion, 2);
+
+		if(GameObject.FindGameObjectsWithTag("Player").Length == 0){
+			GameObject enemy1 = Instantiate (Resources.Load("enemy1", typeof(GameObject))) as GameObject;
+			GameObject enemy2 = Instantiate (Resources.Load("enemy2", typeof(GameObject))) as GameObject;
+			GameObject enemy3 = Instantiate (Resources.Load("enemy3", typeof(GameObject))) as GameObject;
+			GameObject enemy4 = Instantiate (Resources.Load("enemy4", typeof(GameObject))) as GameObject;
+		}
+
+		Destroy (gameObject);
+	}
+}
